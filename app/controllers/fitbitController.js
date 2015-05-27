d251a508b0960038de3a9f5d98968b4acbf26df0
ClaytonSmith <cks_jr@yahoo.com>
2015-05-27 12:54:41 -0400

    Minor update

--- app/controllers/fitbitController.js
+++ app/controllers/fitbitController.js
@@ -1,22 +1,25 @@
 'use strict';
-angular.module('myApp.view1', ['ngRoute', 'leaflet-directive', 'nvd3ChartDirectives'])
 
-    .config(['$routeProvider', function($routeProvider) {
-	$routeProvider.when('/view1', {
+
+var app = angular.module("myApp", ['ngRoute', 'leaflet-directive', 'nvd3ChartDirectives']);
+
+
+/*app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/view1', {
 	    templateUrl: 'view1/view1.html',
-	    controller: 'View1Ctrl'
+	controller: 'View1Ctrl'
 	});
-    }])
+}])*/
 
-.controller('View1Ctrl', ['$scope', function($scope) {
-    angular.extend($scope, {
-        defaults: {
-            scrollWheelZoom: false
-        }
-    });
 
+app.controller("fitbitMapController", [ "$scope", "leafletData", function($scope, leafletData) {
+    // Nothing yet
+}]);
+
+
+/*app.controller('View1Ctrl', ['$scope', function($scope) {
     $scope.exampleData = [{
         key: "Series 1",
         values: [ [ "Brian" , 20000] , [ "Steven", 50] , [ "Clayton", 6000] , [ "Rich" , 10000]    ]
     }];
-}]);
+}]);*/
