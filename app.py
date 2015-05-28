ee684836b6d12fdabe30726a1777fc66ee9aa5e5
Brian Murphy <b.rian@cloudlock.com>
2015-05-27 22:09:22 -0400

    capture the right data and then dispiay all the goods from fitbit

--- app.py
+++ app.py
@@ -23,18 +23,36 @@ def authorize():
 
 @app.route('/thankyou')
 def thank_you():
-    customer_tok = request.args.get('oauth_token')
     customer_ver = request.args.get('oauth_verifier')
 
-    return '{}:{}'.format(customer_tok, customer_ver)
+    client = fitbit.FitbitOauthClient(consumer_key, consumer_secret)
+    token = client.fetch_access_token(customer_ver)
+
+    # store these
+    token.get(u'oauth_token')
+    token.get(u'oauth_token_secret')
+
+    return redirect('http://localhost:8000/app/#/')
 
 
 @app.route('/distance')
 def get_distance_data():
-    return {
-        "Series 1":
-        [["Brian", 20000], ["Steven", 50], ["Clayton", 6000], ["Rich", 10000]]
-    }
+
+    all_users = [['a', 'b']]
+    results = []
+    # fetch all REAL customers
+    for cust_key, cust_tok in all_users:
+        authd_client = fitbit.Fitbit(consumer_key, consumer_secret,
+                                     resource_owner_key=cust_key,
+                                     resource_owner_secret=cust_tok)
+
+        profile = authd_client.user_profile_get()
+        name = profile.get(u'user').get(u'displayName')
+        avatar = profile.get(u'user').get(u'avatar150')
+        stats = authd_client.time_series(u'activities/distance', period=u'1d')
+        distance = stats.get(u'activities-distance')[0].get(u'value')
+        results.append('{},{},{}'.format(name, avatar, distance))
+    return {'Series 1': results}
 
 if __name__ == '__main__':
     app.run(debug=True, port=6544)
